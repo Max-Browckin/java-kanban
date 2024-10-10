443b4e5d5db949dfaff8d68ef0dab43d593da586
Max <m.browckin@yandex.ru>
2024-10-10 21:07:01 +0300

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        TaskManager taskManager = new TaskManager();
+
+        Task packBoxes = new Task("Упаковать вещи", "В коробки и мешки");
+        Task packBoxesCreated = taskManager.addTask(packBoxes);
+        System.out.println(packBoxesCreated);
+
+        Task packBoxesToUpdate = new Task(packBoxes.getId(), "Упаковать вещи быстро", "В коробки и мешки",
+                Status.IN_PROGRESS);
+        Task packBoxesUpdated = taskManager.updateTask(packBoxesToUpdate);
+        System.out.println(packBoxesUpdated);
+
+        Epic moving = new Epic("Переезд", "Нужно успеть до конца месяца");
+        taskManager.addEpic(moving);
+        System.out.println(moving);
+        Subtask packKitchen = new Subtask("Упаковать кухню", "В отдельные коробки",
+                moving.getId());
+        Subtask packBedroom = new Subtask("Упаковать спальню", "В большие коробки",
+                moving.getId());
+        taskManager.addSubtask(packKitchen);
+        taskManager.addSubtask(packBedroom);
+        System.out.println(moving);
+        packBedroom.setStatus(Status.DONE);
+        taskManager.updateSubtask(packBedroom);
+        packKitchen.setStatus(Status.DONE);
+        taskManager.updateSubtask(packKitchen);
+        System.out.println(moving);
     }
 }
