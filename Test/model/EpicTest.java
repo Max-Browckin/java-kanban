08fc2d9bbb2628b4408ec9d71587083f09da6af4
Max <m.browckin@yandex.ru>
2024-10-13 23:55:50 +0300

    commit 5

--- Test/model/EpicTest.java
+++ Test/model/EpicTest.java
@@ -10,8 +10,8 @@ class EpicTest {
 
     @Test
     public void EpicsWithEqualIdShouldBeEqual() {
-        Epic epic1 = new Epic(10, "Сделать ремонт", "Уложиться в 2 миллиона", Status.NEW);
-        Epic epic2 = new Epic(10, "Подготовиться к собеседованию", "1 июля в 11:00",
+        Epic epic1 = new Epic(10, "Забрать заказ", "На Ozon", Status.NEW);
+        Epic epic2 = new Epic(10, "Забрать заказ", "На Wildberries",
                 Status.IN_PROGRESS);
         assertEquals(epic1, epic2,
                 "Ошибка! Наследники класса Task должны быть равны друг другу, если равен их id;");
